a4e5265509e13b3f9a1f46d9ee625a520982b37f
Shuvendu Bikash <bikash11roy@gmail.com>
2017-10-06 15:17:46 +0600

    making lookup table
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- Word2Vec.py
+++ Word2Vec.py
@@ -13,3 +13,7 @@ words = utils.preprocess(text)
 print("Total words: {}".format(len(words)))
 print("Unique words: {}".format(len(set(words))))
 
+
+# making a look up table
+vocab_to_int, int_to_vocab = utils.create_lookup_tables(words)
+
