2a5d794bf2fabe706dfa11a9253911492ca1100c
Shuvendu Bikash <bikash11roy@gmail.com>
2017-10-06 15:51:01 +0600

    subsampling
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- Word2Vec.py
+++ Word2Vec.py
@@ -17,3 +17,31 @@ print("Unique words: {}".format(len(set(words))))
 # making a look up table
 vocab_to_int, int_to_vocab = utils.create_lookup_tables(words)
 
+# converting the entire data represented in form of foken number
+int_words = [vocab_to_int[word] for word in words]
+
+
+
+# Subsampling
+from collections import Counter
+import random
+
+threshold = 1e-5
+number_of_words = len(int_words)
+word_counter = Counter(int_words)
+
+frequencies = dict()
+drop_probabilities = dict()
+train_words = []
+
+droped_count = 0
+for word, count in word_counter.items():
+    frequency = count/number_of_words
+    frequencies[word] = frequency
+    drop_probabilities[word] = 1 - np.sqrt(threshold/frequency)
+    
+for word in int_words:
+    if drop_probabilities[word] < 0.85:
+        train_words.append(word)
+        
+print(len(train_words))
