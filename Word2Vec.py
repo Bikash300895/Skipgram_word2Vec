3e398eebdacd540e295df4a450ce895fe83d2e07
Shuvendu Bikash <bikash11roy@gmail.com>
2017-10-14 05:22:58 +0600

    defing the grapg
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- Word2Vec.py
+++ Word2Vec.py
@@ -91,3 +91,105 @@ def get_batches(words, batch_size, window_size=5):
             y.extend(batch_y)
             x.extend([batch_x]*len(batch_y))
         yield x, y
+
+
+# defining the graph
+train_graph = tf.Graph()
+with train_graph.as_default():
+    inputs = tf.placeholder(tf.int32,shape=[None],name = "inputs")
+    labels = tf.placeholder(tf.int32,shape = [None,1],name = "labels")
+    
+    
+n_vocab = len(int_to_vocab)
+n_embedding =  300 
+with train_graph.as_default():
+    embedding = tf.Variable(tf.random_uniform([n_vocab,n_embedding],-1,-1 ))
+    embed = tf.nn.embedding_lookup(embedding,inputs)
+
+
+# Number of negative labels to sample
+n_sampled = 100
+with train_graph.as_default():
+    softmax_w = tf.Variable(tf.truncated_normal( [n_vocab,n_embedding],stddev=0.1),name = "softmax_w")
+    softmax_b = tf.Variable( tf.zeros([n_vocab]),name = "softmax_b")
+    
+    # Calculate the loss using negative sampling
+    loss = tf.nn.sampled_softmax_loss(softmax_w,softmax_b,labels,embed,n_sampled,n_vocab)
+    
+    cost = tf.reduce_mean(loss)
+    optimizer = tf.train.AdamOptimizer().minimize(cost)   
+    
+    
+    
+    
+# Validation
+with train_graph.as_default():
+    ## From Thushan Ganegedara's implementation
+    valid_size = 16 # Random set of words to evaluate similarity on.
+    valid_window = 100
+    # pick 8 samples from (0,100) and (1000,1100) each ranges. lower id implies more frequent 
+    valid_examples = np.array(random.sample(range(valid_window), valid_size//2))
+    valid_examples = np.append(valid_examples, 
+                               random.sample(range(1000,1000+valid_window), valid_size//2))
+
+    valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+    
+    # We use the cosine distance:
+    norm = tf.sqrt(tf.reduce_sum(tf.square(embedding), 1, keep_dims=True))
+    normalized_embedding = embedding / norm
+    valid_embedding = tf.nn.embedding_lookup(normalized_embedding, valid_dataset)
+    similarity = tf.matmul(valid_embedding, tf.transpose(normalized_embedding))
+    
+
+# Training
+epochs = 10
+batch_size = 1000
+window_size = 10
+
+with train_graph.as_default():
+    saver = tf.train.Saver()
+
+with tf.Session(graph=train_graph) as sess:
+    iteration = 1
+    loss = 0
+    sess.run(tf.global_variables_initializer())
+
+    for e in range(1, epochs+1):
+        batches = get_batches(train_words, batch_size, window_size)
+        start = time.time()
+        for x, y in batches:
+            
+            feed = {inputs: x,
+                    labels: np.array(y)[:, None]}
+            train_loss, _ = sess.run([cost, optimizer], feed_dict=feed)
+            
+            loss += train_loss
+            
+            if iteration % 100 == 0: 
+                end = time.time()
+                print("Epoch {}/{}".format(e, epochs),
+                      "Iteration: {}".format(iteration),
+                      "Avg. Training loss: {:.4f}".format(loss/100),
+                      "{:.4f} sec/batch".format((end-start)/100))
+                loss = 0
+                start = time.time()
+            
+            if iteration % 1000 == 0:
+                ## From Thushan Ganegedara's implementation
+                # note that this is expensive (~20% slowdown if computed every 500 steps)
+                sim = similarity.eval()
+                for i in range(valid_size):
+                    valid_word = int_to_vocab[valid_examples[i]]
+                    top_k = 8 # number of nearest neighbors
+                    nearest = (-sim[i, :]).argsort()[1:top_k+1]
+                    log = 'Nearest to %s:' % valid_word
+                    for k in range(top_k):
+                        close_word = int_to_vocab[nearest[k]]
+                        log = '%s %s,' % (log, close_word)
+                    print(log)
+            
+            iteration += 1
+    save_path = saver.save(sess, "checkpoints/text8.ckpt")
+    embed_mat = sess.run(normalized_embedding)
+
+
