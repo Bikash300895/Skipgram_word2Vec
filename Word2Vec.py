91e7dafff0d4c4ab3c9af9d8ce4a954b54e8c07f
Shuvendu Bikash <bikash11roy@gmail.com>
2017-10-13 20:45:41 +0600

    preprocessing
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- Word2Vec.py
+++ Word2Vec.py
@@ -5,16 +5,19 @@ from random import randint
 import time
 
 with open('data/text8') as f:
-    text = f.read()
+    text = f.read()                 # all sequential text data
     
 # Preprocessing the data
+# process the raw data, replace systex with text and return a list in sequence of word
 words = utils.preprocess(text)
 
-print("Total words: {}".format(len(words)))
-print("Unique words: {}".format(len(set(words))))
+print("Total words: {}".format(len(words)))             # 16,680,599
+print("Unique words: {}".format(len(set(words))))       # 63,641
 
 
 # making a look up table
+# vocab_to_int['a'] = 5, which the index of token 'a'
+# int_to_vocab[5] = 'a'
 vocab_to_int, int_to_vocab = utils.create_lookup_tables(words)
 
 # converting the entire data represented in form of foken number
@@ -39,9 +42,13 @@ for word, count in word_counter.items():
     frequency = count/number_of_words
     frequencies[word] = frequency
     drop_probabilities[word] = 1 - np.sqrt(threshold/frequency)
-    
+
+# Keeping the word with word probability < .85    
 for word in int_words:
     if drop_probabilities[word] < 0.85:
         train_words.append(word)
         
 print(len(train_words))
+
+
+
